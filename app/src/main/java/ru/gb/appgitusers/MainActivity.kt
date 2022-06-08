ac25df94ae111c34874f2b2439e94debcd8bcd12
Vladimir Petrov <vl.pva@yandex.ru>
2022-06-08 20:51:36 +0300

    add layout

--- app/src/main/java/ru/gb/appgitusers/MainActivity.kt
+++ app/src/main/java/ru/gb/appgitusers/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.gb.appgitusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.gb.appgitusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
